019fb9e6e162a4a0b5b44afb0ed6f2a48572447a
Merlin2010 <Tobias@virtual-pahl.de>
2017-07-21 00:00:08 +0200

    int

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ def index():
 	return 'Yo, its working!'
 if __name__ == "__main__":
 	from os import environ
-	app.run(debug=True, host='0.0.0.0', port=environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=int(environ.get("PORT", 5000)))
 	
