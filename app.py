6296048795e760f4625dc7278afd2eb0ebccf592
Merlin2010 <Tobias@virtual-pahl.de>
2017-07-20 23:42:58 +0200

    port

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ app = Flask(__name__)
 def index():
 	return 'Yo, its working!'
 if __name__ == "__main__":
-	app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
